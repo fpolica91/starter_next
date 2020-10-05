d665b7bbcbf32e8257278679b38d9afc8a256c4e
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2020-10-04 22:32:45 -0400

    product page done

--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,9 +1,52 @@
 import { Title } from '@/styles/Pages/home'
+import Prismic from 'prismic-javascript'
+import Link from 'next/link'
+import PrismicDOM from 'prismic-dom'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
+import { client } from '@/lib/prismic'
+import { Document } from 'prismic-javascript/types/documents'
 
-export default function Product() {
+interface ProductProps {
+  product: Document
+}
+
+export default function Product({ product }: ProductProps) {
+  const router = useRouter()
+  if (router.isFallback) {
+    return <p>Loading....</p>
+  }
+
   return (
-    <>
-      <Title>Product</Title>
-    </>
+    <div>
+      <Title>{PrismicDOM.RichText.asText(product.data.title)}</Title>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: PrismicDOM.RichText.asHtml(product.data.description),
+        }}
+      ></div>
+      <img src={product.data.thumbnail.url} alt="product image" width="300" />
+      <p>Price: ${product.data.price}</p>
+    </div>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    // fallback will try to find any page that has not been created
+    // if fallback false, will return not found.
+    paths: [],
+    fallback: true,
+  }
+}
+
+export const getStaticProps: GetStaticProps<ProductProps> = async (context) => {
+  const { slug } = context.params
+  const product = await client().getByUID('product', String(slug), {})
+  return {
+    props: {
+      product,
+    },
+    revalidate: 10,
+  }
 }
